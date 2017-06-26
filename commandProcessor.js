80c78adfc901e017f2baf0ed883b5e9f579db7a4
Hoang Ngoc Nghia <nghia@motionportrait.com>
2017-06-26 11:04:02 +0700

    module for processing command

---
+++ commandProcessor.js
@@ -0,0 +1,43 @@
+const gCheckInOutRegex = /#(in|out){1} ?(([0-9]|0[0-9]|1[0-9]|2[0-3]):([0-5][0-9]))?/i
+let CommandInf = {
+    error: {
+        code: 0,
+        msg: ''
+    },
+    timestamp: '',
+    usrName: '',
+    usrId: '',
+    action: '', // in or out
+    hh: '',
+    mm: ''
+}
+
+module.exports = function processCommand(session) {
+    let cmdArr
+    var cmd = Object.create(CommandInf)
+    let msg = session.message.text
+    // cmdArr = gCheckInOutRegex.exec(msg)
+    cmdArr = msg.match(gCheckInOutRegex)
+    // if(gCheckInOutRegex.test(msg)) {
+    
+    console.log(`Arr: ${cmdArr}`)
+    if(cmdArr && cmdArr.length > 0) {
+        if(cmdArr[2] && cmdArr[2] != ''){
+            // session.send(`${session.message.timestamp}:${session.message.user.name}:[${cmdArr[1]}:${cmdArr[2]}:${cmdArr[3]}]`)            
+            cmd.error.code = 0
+            cmd.timestamp = session.message.timestamp
+            cmd.usrName = session.message.user.name
+            cmd.usrId = session.message.user.id
+            cmd.action = cmdArr[1]
+            cmd.hh = cmdArr[3]
+            cmd.mm = cmdArr[4]
+        } else {
+            cmd.error.code = 1;
+            cmd.error.msg = `"${cmdArr[0]}..." not a valid command!`
+        }
+    } else {
+        cmd.error.code = -1;
+        cmd.error.msg = 'Not a command'
+    }
+    return cmd
+}
